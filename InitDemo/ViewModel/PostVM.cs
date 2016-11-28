f212bdef8bf07805f4f05e1f257dd16985439d87
kris <kristiyanjordanov@gmail.com>
2016-11-28 11:55:28 +0200

    added post controller and views

--- InitDemo/ViewModel/PostVM.cs
+++ InitDemo/ViewModel/PostVM.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,6 +8,11 @@ namespace InitDemo.ViewModel
 {
     public class PostVM
     {
-
+        
+        public int Id { get; set; }
+        [StringLength(100, ErrorMessage = "error" ,MinimumLength =2)]
+        public string Name { get; set; }
+        public DateTime DateCreated { get; set; }
+        public String Content { get; set; }
     }
 }
